8e83836e3e414eebb4b394c99531dd609913c7ad
Utkarsh Maurya <115497430+utkarshmaurya1211@users.noreply.github.com>
2024-08-03 13:24:51 +0000

    Date and Time

---
+++ 01_basics/dates.js
@@ -0,0 +1,8 @@
+let myDate= new Date()
+console.log(typeof myDate);
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleDateString());
+
+let createmyDate= new Date(2023,0,12)
+console.log(createmyDate.toDateString());
