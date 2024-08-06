778a5cb365659cb7e0289858a5c8c14dbcdd65a2
Utkarsh Maurya <115497430+utkarshmaurya1211@users.noreply.github.com>
2024-08-06 19:46:34 +0000

    date update done

--- projects_basics/project_3/javascript.js
+++ projects_basics/project_3/javascript.js
@@ -1,9 +1,14 @@
 const clock = document.getElementById('clock');
 // const clock = document.querySelector('#clock')
-
+const date= document.querySelector('#date')
 setInterval(function () {
   let date = new Date();
+  
   // console.log(date.toLocaleTimeString());
   clock.innerHTML = date.toLocaleTimeString();
+ 
 }, 1000);
-
+setInterval(function(){
+  let time= new Date();
+  date.innerHTML= time.toLocaleDateString();
+},1000);
